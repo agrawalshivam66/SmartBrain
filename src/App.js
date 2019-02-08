0ccdef68b57d024012134bdd509dcdeb3c66080b
Shivam Agrawal <agrawalshivam66@gmail.com>
2019-02-08 19:28:41 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'tachyons'
+import Particles from 'react-particles-js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+      
+                
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Particles className='particles'
+                params={particlesOptions} 
+                />
       <Navigation/>
       <Logo/>
       <Rank/>
