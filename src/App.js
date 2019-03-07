bb190f3609742ee54f0c4e6772e7d8cb1a6269e5
Shivam Agrawal <agrawalshivam66@gmail.com>
2019-03-08 01:15:23 +0530

    all basic buttons working

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Navigation from './components/Navigation/Navigation';
 const particlesOptions = {
   particles: {
     number: {
-      value: 30,
+      value: 80,
       density: {
         enable: true,
         value_area: 800
@@ -22,6 +22,19 @@ const particlesOptions = {
                 
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input:'',
+    }
+  }
+  onInputChange = (event) =>{
+    console.log(event.target.value);
+  }
+  onButtonSubmit = () =>{
+    console.log('click');
+  }
+ 
   render() {
     return (
       <div className="App">
@@ -31,7 +44,8 @@ class App extends Component {
       <Navigation/>
       <Logo/>
       <Rank/>
-      <ImageLinkForm/>{/*
+      <ImageLinkForm onInputChange={this.onInputChange}
+       onButtonSubmit={this.onButtonSubmit}/>{/*
       <FaceRecognition/>*/}
       </div>
     );
