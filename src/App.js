5259e2572e2b1c1c001442b42472526beb11aace
Shivam Agrawal <agrawalshivam66@gmail.com>
2019-03-08 23:55:37 +0530

    sample clarify api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import 'tachyons'
+import 'tachyons';
 import Particles from 'react-particles-js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
+import Clarifai from 'clarifai';
 
 const particlesOptions = {
   particles: {
@@ -18,8 +19,12 @@ const particlesOptions = {
     }
   }
 }
-      
-                
+
+
+// initialize with your api key. This will also work in your browser via http://browserify.org/      
+const app = new Clarifai.App({
+ apiKey: '71ea42b76fe340ddac5613e213bd3363'
+});                
 
 class App extends Component {
   constructor(){
@@ -32,7 +37,14 @@ class App extends Component {
     console.log(event.target.value);
   }
   onButtonSubmit = () =>{
-    console.log('click');
+    app.models.predict("71ea42b76fe340ddac5613e213bd3363", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      // do something with response
+    },
+    function(err) {
+      // there was an error
+    }
+  );
   }
  
   render() {
