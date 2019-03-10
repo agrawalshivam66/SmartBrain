65c2c3d39e224ec86cd4593c5422b9266237b37f
Shivam Agrawal <agrawalshivam66@gmail.com>
2019-03-10 23:04:32 +0530

    update

--- src/App.js
+++ src/App.js
@@ -32,17 +32,19 @@ class App extends Component {
     super();
     this.state = {
       input:'',
+      imageUrl:''
     }
   }
   onInputChange = (event) =>{
-    console.log(event.target.value);
+    this.setState({input:event.target.value});
   }
   onButtonSubmit = () =>{
-    app.models.predict("f831ec3c4d674790830a07604d0e7611", 
-      "https://samples.clarifai.com/face-det.jpg").then(
+    this.setState({imageUrl: this.state.input})
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, 
+      this.state.input).then(
     function(response) {
       // do something with response
-      console.log(response)
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
     },
     function(err) {
       // there was an error
@@ -62,7 +64,7 @@ class App extends Component {
       <Rank/>
       <ImageLinkForm onInputChange={this.onInputChange}
        onButtonSubmit={this.onButtonSubmit}/>{
-      <FaceRecognition/>}
+      <FaceRecognition imageUrl={this.state.imageUrl}/>}
       </div>
     );
   }
